05efd101d9c14115ff0e47bd0f7106ad0cfc811b
ravi-rana <ranaravishanker@gmail.com>
2016-09-18 23:44:50 +0530

    changed output string

--- read_string.c
+++ read_string.c
@@ -3,7 +3,7 @@
 int main() {
 	char name[20]; // string = array of character 
 	scanf("%s",name);  // scaning char  array
-	printf("Your name is %s.",name);
+	printf("Hi %s\n",name);
 	
 	return 0;
 }
