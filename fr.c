a8b9b3520baba32fd017d1f52fbe7b4a71f6119a
ravi-rana <ranaravishanker@gmail.com>
2016-09-19 01:57:25 +0530

    loop bug fixed

--- fr.c
+++ fr.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int a;
-	scanf("%d",&a);
-	for(int b=1;b<=10;b++)
-	{
-		int b=a*b;
-	        printf("%d*%d=%d\n",a,b,b);
+	int a, b;
+	scanf("%d", &a);
+	for(b = 1; b <= 10; b++)
+	{	
+		int res = a*b;
+
+	        printf("%d * %d = %d\n",a,b,res);
 	}
 	return 0;
 }
