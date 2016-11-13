247fa40793a7ce81ce1757458af07795d715564a
RB <rb.rana1993@gmail.com>
2016-11-13 22:20:58 +0530

    symbol program if else /

---
+++ symbol.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	int x;
+	scanf("%d", &x);
+	if(x == 10) {
+		printf("hey i am 10\n");
+	}else if(x == 5){
+		printf("hey i am 5\n");
+	}else if(x == 11){
+		printf("hey i am 11 \n");
+	}else {
+		printf("i don't know who i am \n");
+	}
+
+	return 0;
+}
+
+
+
