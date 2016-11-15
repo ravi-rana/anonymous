1e616683ff6ecb776d00aa1e5527d7d782a9f921
RB <rb.rana1993@gmail.com>
2016-11-15 21:03:25 +0530

    function importance

--- FLOW001.c
+++ FLOW001.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 //https://www.codechef.com/problems/FLOW001
-int main()
-{
-	int t;
-	int a, b;
-	scanf("%d", &t);
-        t--;
-	do{
-		scanf("%d %d", &a, &b);
-		printf("%d\n", a + b);	
-	}while(t--);
 
-	return 0;
-
+int Addbyme(int simple1, int simple2) {
+	return simple1 + simple2;
 }
+int tablesum(int number) {
+	int sum = 0;
+	int i;
+	for(i =  0; i <= 10; i++) {
+		printf("%d\n", number * i);
+		sum += (number * i);
+	}
+	return sum;
+}
+
+int main()
+
+	int z;
+	z = tablesum(2);
+	printf("sum = %d\n", z);
+	z = tablesum(12);
+	printf("sum = %d\n", z);
+}
+
+
