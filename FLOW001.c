4c3de3b81cd0f274aa270ba9bc6e0325e3a81e15
RB <rb.rana1993@gmail.com>
2016-11-15 20:11:46 +0530

    simple adding Question

---
+++ FLOW001.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//https://www.codechef.com/problems/FLOW001
+int main()
+{
+	int t;
+	int a, b;
+	scanf("%d", &t);
+	while(t--){
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);	
+	}
+
+	return 0;
+
+}
